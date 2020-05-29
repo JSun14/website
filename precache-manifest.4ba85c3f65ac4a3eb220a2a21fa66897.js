5acf428f79b3f3416cfebd95221e89ec524f6c48
Jerry Sun <js2746@cornell.edu>
2020-05-28 18:38:49 -0700

    Updates

---
+++ precache-manifest.4ba85c3f65ac4a3eb220a2a21fa66897.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39a36107ef9285ba4932ecafc193203a",
+    "url": "/JSun14.github.io/website/index.html"
+  },
+  {
+    "revision": "0e2b06d69387cf461889",
+    "url": "/JSun14.github.io/website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d3b3b7e629bf9bd4d3c8",
+    "url": "/JSun14.github.io/website/static/js/2.df5d811d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JSun14.github.io/website/static/js/2.df5d811d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e2b06d69387cf461889",
+    "url": "/JSun14.github.io/website/static/js/main.07e3fb9f.chunk.js"
+  },
+  {
+    "revision": "6dc6956e3a2aeee87487",
+    "url": "/JSun14.github.io/website/static/js/runtime-main.b3f69449.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/JSun14.github.io/website/static/media/logo.5d5d9eef.svg"
+  }
+]);
