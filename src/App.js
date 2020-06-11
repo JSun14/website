cbb45c8eb0522f0671906ec0cb47946d6ac328d7
Jerry Sun <js2746@cornell.edu>
2020-06-10 23:41:51 -0700

    Header and intro'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Typography, Toolbar, AppBar, IconButton, Card, CardContent, Grid } from '@material-ui/core';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import style from './styling.module.css';
+import logo from './static/logo.png';
+import doge from './static/doge.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div>
+
+        <AppBar className={style.toolbar} position='relative'>
+          <Toolbar className={style.toolbar}>
+            <IconButton color="inherit" target="_blank" href="https://github.com/JSun14">
+              <GitHubIcon fontSize="large" />
+            </IconButton>
+            <img src={logo} className={style.logo} />
+            <IconButton color="inherit" target="_blank" href="https://www.linkedin.com/in/jerry-sun-50b660182/">
+              <LinkedInIcon fontSize="large" />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Grid container xs={12}>
+          <Grid item xs={5} >
+            <Card elevation={0} square={true} >
+              <CardContent className={style.intro}>
+                <Typography variant="h4" className={style.whitetext}>
+                  <b>Hi I'm Jerry. Welcome to my website. It's currently under developement.</b>
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={7}>
+            <Card elevation={0} square={true}>
+              <CardContent >
+                <img src={doge} className={style.doge} />
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </div>
+    </React.Fragment>
   );
 }
 
